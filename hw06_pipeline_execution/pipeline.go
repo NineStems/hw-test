533d0c62ed00604e1bfb6fbc2c7edd84d1812e78
NineStems <ninestems@icloud.com>
2022-07-25 23:25:27 +0300

    HW6 is not yet completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,31 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return nil
+	}
+	out := in
+	for _, stage := range stages {
+		out = next(done, stage(out))
+	}
+	return out
+}
+
+func next(done, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case data, open := <-in:
+				if !open {
+					return
+				}
+				out <- data
+			}
+		}
+	}()
+	return out
 }
