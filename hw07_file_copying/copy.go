476b383290b2a4aafb1866358e90892a35bda338
NineStems <ninestems@icloud.com>
2022-08-22 08:10:25 +0300

    add first part of task

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,39 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	ff, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer ff.Close()
+
+	stat, err := ff.Stat()
+	if err != nil {
+		return err
+	}
+
+	if stat.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > stat.Size() {
+		limit = stat.Size()
+	}
+
+	bar := pb.Full.Start64(limit)
+	sectionRead := io.NewSectionReader(ff, offset, limit)
+
+	ft, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer ft.Close()
+
+	_, err = io.Copy(ft, bar.NewProxyReader(sectionRead))
+	if err != nil {
+		return err
+	}
+	bar.Finish()
+
 	return nil
 }
