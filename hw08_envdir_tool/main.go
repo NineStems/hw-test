5c2631bf0481aba594bddaba4b08d34bca93d491
Rodion Sychev <rodion.sychev@south.rt.ru>
2022-08-24 15:58:49 +0300

    del sycn and fix code by linter

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -6,12 +6,12 @@ import (
 	"os"
 )
 
-var ToLittleArgs = fmt.Errorf("count of cmd arguments is less than 2")
+var ErrToLittleArgs = fmt.Errorf("count of cmd arguments is less than 2")
 
 func main() {
 	args := os.Args[1:]
 	if len(args) < 2 {
-		log.Fatal(ToLittleArgs)
+		log.Fatal(ErrToLittleArgs)
 	}
 	envs, err := ReadDir(args[0])
 	if err != nil {
