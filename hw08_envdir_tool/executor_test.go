a21eef6a9a23ec6a298d307016faf42d422c8710
Rodion Sychev <rodion.sychev@south.rt.ru>
2022-08-25 19:44:19 +0300

    added ut

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,50 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	code := RunCmd([]string{"ls"}, nil)
+	if code != 0 {
+		t.Errorf("RunCmd() = %v, want %v", code, 0)
+	}
+}
+
+func TestBadRunCmd(t *testing.T) {
+	code := RunCmd([]string{""}, nil)
+	if code != -1 {
+		t.Errorf("RunCmd() = %v, want %v", code, -1)
+	}
+}
+
+func TestSetEnvRunCmd(t *testing.T) {
+	os.Setenv("EMPTY", "empty")
+	os.Setenv("BAR", "notBar")
+	empty := os.Getenv("EMPTY")
+	bar := os.Getenv("BAR")
+	if empty != "empty" {
+		t.Errorf("empty = %v, want %v", empty, "empty")
+	}
+	if bar != "notBar" {
+		t.Errorf("bar = %v, want %v", bar, "notBar")
+	}
+
+	envs := Environment{
+		"EMPTY": EnvValue{Value: "", NeedRemove: true},
+		"BAR":   EnvValue{Value: "bar"},
+	}
+	code := RunCmd([]string{"ls"}, envs)
+	if code != 0 {
+		t.Errorf("RunCmd() = %v, want %v", code, 0)
+	}
+	empty = os.Getenv("EMPTY")
+	bar = os.Getenv("BAR")
+	if empty != "" {
+		t.Errorf("empty = %v, want %v", empty, "")
+	}
+	if bar != "bar" {
+		t.Errorf("bar = %v, want %v", bar, "bar")
+	}
 }
