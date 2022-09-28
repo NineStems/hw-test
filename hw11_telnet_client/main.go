634754e3b65c4ea4533fd4c1e2818bb4ee3ac43b
Rodion Sychev <rodion.sychev@south.rt.ru>
2022-09-28 15:09:23 +0300

    fix changes

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -4,8 +4,12 @@ package main
 // command-line flag parsing. We'll use this package to
 // implement our example command-line program.
 import (
+	"context"
 	"flag"
 	"log"
+	"net"
+	"os/signal"
+	"syscall"
 	"time"
 )
 import "fmt"
@@ -22,8 +26,37 @@ func main() {
 	}
 
 	host, port := args[0], args[1]
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer cancel()
 
-	fmt.Println("timeout:", timeout.String())
-	fmt.Println("host:", host)
-	fmt.Println("port:", port)
+	addr := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(addr, *timeout, nil, nil)
+	defer client.Close()
+
+	err := client.Connect()
+	if err != nil {
+		log.Fatal(fmt.Errorf("client.Connect: %w", err))
+	}
+
+	go func() {
+		defer cancel()
+
+		if err := client.Send(); err != nil {
+			log.Fatal(fmt.Errorf("client.Send: %w", err))
+			return
+		}
+	}()
+
+	go func() {
+		defer cancel()
+
+		if err := client.Receive(); err != nil {
+			log.Fatal(fmt.Errorf("client.Receive: %w", err))
+			return
+		}
+	}()
+
+	<-ctx.Done()
 }
