23bb6a26e516ddaae1ea3761ecf5711df2f2b3f7
Rodion Sychev <rodion.sychev@south.rt.ru>
2022-09-28 11:35:55 +0300

    save cmd params logic

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,29 @@
 package main
 
+// Go provides a `flag` package supporting basic
+// command-line flag parsing. We'll use this package to
+// implement our example command-line program.
+import (
+	"flag"
+	"log"
+	"time"
+)
+import "fmt"
+
+var NotEnoughArgs = fmt.Errorf("not enouth arguments for establisment connection")
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", time.Second*10, "a duration")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 2 {
+		log.Fatal(NotEnoughArgs)
+	}
+
+	host, port := args[0], args[1]
+
+	fmt.Println("timeout:", timeout.String())
+	fmt.Println("host:", host)
+	fmt.Println("port:", port)
 }
